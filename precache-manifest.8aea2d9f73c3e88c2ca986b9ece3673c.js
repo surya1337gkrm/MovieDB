438f13db7a918875b8d04e3d1e574f8433663551
surya1337gkrm <suryavenkatesh0@gmail.com>
2020-08-14 23:19:11 +0530

    Updates

---
+++ precache-manifest.8aea2d9f73c3e88c2ca986b9ece3673c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8809042d3fdb787977b132fb7ad6339",
+    "url": "/MovieDB/index.html"
+  },
+  {
+    "revision": "7e683c3df9ec140bccfd",
+    "url": "/MovieDB/static/css/main.08480bd0.chunk.css"
+  },
+  {
+    "revision": "87d39f7dbc16620c0f53",
+    "url": "/MovieDB/static/js/2.7b250081.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MovieDB/static/js/2.7b250081.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e683c3df9ec140bccfd",
+    "url": "/MovieDB/static/js/main.fb558d34.chunk.js"
+  },
+  {
+    "revision": "430bc99026950e34d4b0",
+    "url": "/MovieDB/static/js/runtime-main.ac2c5693.js"
+  }
+]);
